1db353cccb505884b58e5ed511f0e6cdb0431e31
HelenaCSU <35887045+HelenaCSU@users.noreply.github.com>
2018-02-08 15:22:55 +0800

    Update resize.py

--- resize.py
+++ resize.py
@@ -10,8 +10,8 @@ import os
 import sys
 #%%
 '''
-对图片进行缩放
-Times为缩放的倍数
+对图片进行缩放(既可以是缩小到指定的width，height； 也可以缩小到Times(倍数)
+
 os.listdir() 是返回指定目录下的所有文件和目录名
 os.path.join(path,name)是连接目录与文件名
 os.path.splitext() 是分离文件名和扩展名
@@ -24,6 +24,8 @@ def resize():
        if item.split('.')[-1]=='jpg':
             item_path=os.path.join(path,item)
             im=Image.open(item_path)
+            #(width,height)=im.size
+            #imResize1=im.resize((width*Times,height*Times),Image.ANTIALIAS)
             f,e=os.path.splitext(item_path)
             imResize=im.resize((224,224),Image.ANTIALIAS)
             imResize.save(f+'resized.jpg','JPEG', quality=90)
